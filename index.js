2ca3b0419545e411b80de47f17fd8e758257d786
Alex Gunnarsson <alexgunnarsson@live.se>
2022-12-07 22:32:19 +0100

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const fetch = (...args) =>
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+const express = require("express")
+require("dotenv").config()
+const PORT = process.env.PORT
+const API_KEY = process.env.API_KEY
+
+const app = express()
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log("Starting server at port " + PORT)
+})
+app.use(express.static("public"))
