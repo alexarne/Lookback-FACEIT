9949153d00225465c0e504f9641db28778a5700c
Alex Gunnarsson <alexgunnarsson@live.se>
2022-12-11 03:16:44 +0100

    halve the amount of api calls

--- index.js
+++ index.js
@@ -29,38 +29,44 @@ app.post("/mutualGames", async (req, res) => {
         error(res, "Incorrect body")
         return
     }
+    if (equalStrings(user1, user2)) {
+        error(res, "Names can't be same")
+        return
+    }
 
     // Get user IDs
     let responses = await Promise.all([
-        fetch(`https://open.faceit.com/data/v4/search/players?nickname=${user1}&offset=0&limit=1`),
-        fetch(`https://open.faceit.com/data/v4/search/players?nickname=${user2}&offset=0&limit=1`)
+        fetch(`https://open.faceit.com/data/v4/search/players?nickname=${user1}&offset=0&limit=100`),
+        fetch(`https://open.faceit.com/data/v4/search/players?nickname=${user2}&offset=0&limit=100`)
     ])
     if (responses[0].status !== 200 || responses[1].status !== 200) {
         if (responses[1].status === 429) {
             error(res, "Too many requests, retry after " + responses[1].headers.get("retry-after") + " seconds")
             return
         }
-        console.log("Unknown Error " + responses[0].status, responses[0])
+        if (responses[0].status !== 200) console.log(`Unknown Error (user1='${user1}'): ` + responses[0].status, responses[0])
+        if (responses[1].status !== 200) console.log(`Unknown Error (user2='${user2}'): ` + responses[1].status, responses[1])
         error(res, "Fetch userIDs, codes " + responses[0].status + ", " + responses[1].status)
         return
     }
     let data = await Promise.all(responses.map(r => r.json()))
     if (responses[0].status !== 200 || data[0].items.length == 0 || !equalStrings(data[0].items[0].nickname, user1)) {
-        error(res, "User '" + user1 + "' doesn't exist")
+        error(res, "Cannot find user '" + user1 + "'")
         return
     }
     if (responses[1].status !== 200 || data[1].items.length == 0 || !equalStrings(data[1].items[0].nickname, user2)) {
-        error(res, "User '" + user2 + "' doesn't exist")
+        error(res, "Cannot find user '" + user2 + "'")
         return
     }
-    const user1_id = data[0].items[0].player_id
-    const user2_id = data[1].items[0].player_id
+
+    // First name in list is a soft match (without capitalization), seek true match
+    const user1_id = findUserID(user1, data[0].items)
+    const user2_id = findUserID(user2, data[1].items)
 
     // Get recent {count} matches for each user
     const queries = []
     for (let i = 0; i*100 < count; i++) {
         queries[i] = fetch(`https://open.faceit.com/data/v4/players/${user1_id}/history?game=${game}&offset=${i*100}&limit=100`)
-        queries[count/100 + i] = fetch(`https://open.faceit.com/data/v4/players/${user2_id}/history?game=${game}&offset=${i*100}&limit=100`)
     }
     responses = await Promise.all(queries)
     data = await Promise.all(responses.map(function(r) {
@@ -71,31 +77,46 @@ app.post("/mutualGames", async (req, res) => {
         }
     }))
 
-    let user1_matches = []
-    let user2_matches = []
-    for (let i = 0; i*100 < count; i++) {
-        if (responses[i].status === 200) user1_matches = user1_matches.concat(data[i].items)
-        if (responses[count/100 + i].status === 200) user2_matches = user2_matches.concat(data[count/100 + i].items)
-    }
-
     const mutual_matches = []
-    let c1 = 0, c2 = 0
-    while (c1 < user1_matches.length && c2 < user2_matches.length) {
-        if (user1_matches[c1].started_at > user2_matches[c2].started_at) {
-            c1++
-        } else if (user1_matches[c1].started_at < user2_matches[c2].started_at) {
-            c2++
-        } else {
-            if (user1_matches[c1].match_id === user2_matches[c2].match_id) {
-                mutual_matches.push(user1_matches[c1])
-            }
-            c1++
-            c2++
+    for (let i = 0; i*100 < count; i++) {
+        if (responses[i].status === 200) {
+            data[i].forEach(match => {
+                if (match.playing_players.includes(user2_id)) {
+                    mutual_matches.push(match)
+                }
+            })
         }
     }
+
+    // let user1_matches = []
+    // for (let i = 0; i*100 < count; i++) {
+    //     if (responses[i].status === 200) user1_matches = user1_matches.concat(data[i].items)
+    // }
+
+    // const mutual_matches = []
+    // user1_matches.forEach(match => {
+    //     if (match.playing_players.includes(user2_id)) {
+    //         mutual_matches.push(match)
+    //     }
+    // })
+
     console.log(mutual_matches.length)
     res.json(mutual_matches)
 })
+
+// Problem: Players with same username but different capitalization can exist
+// Solution: Compare first 100 users in list, take best fit or first result (can miss a true match if >100 names)
+function findUserID(user, data) {
+    let user_id
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].nickname === user) {
+            user_id = data[i].player_id
+            break
+        }
+    }
+    if (user_id == undefined) user_id = data[0].player_id
+    return user_id
+}
 
 function error(res, message) {
     res.status(400)
--- public/script.js
+++ public/script.js
@@ -4,10 +4,20 @@ const URL = window.location.href.slice(0, -1)   // Remove last '/'
 async function displayMutualGames() {
     const field1 = document.getElementById("input-user1-text")
     const field2 = document.getElementById("input-user2-text")
+    const game = document.getElementById("input-game-dropdown")
     const button = document.getElementById("input-submit")
     const status = document.getElementById("input-status")
+
+    button.disabled = true
     status.innerHTML = "Loading..."
-    const [status_code, games] = await getMutualGames(field1.value, field2.value, 1000, "lol_EUW")
+    const [status_code, games] = await getMutualGames(
+        field1.value, 
+        field2.value, 
+        1000, 
+        game.value
+    )
+    // Maximum value for count is somewhere between 38,000 and 40,000. Beyond that is rate limited
+
     if (status_code === 200) {
         status.innerHTML = ""
 
@@ -16,7 +26,7 @@ async function displayMutualGames() {
 
     }
     console.log(games)
-
+    button.disabled = false
 }
 
 async function getMutualGames(user1, user2, count, game) {
